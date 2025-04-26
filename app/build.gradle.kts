da3f7df1f70b73aba5ce269c1c27dffcb3d05f21
Saisathish Karthikeyan <ksaisathish@yahoo.com>
2025-04-26 17:30:28 +0530

    Updated few other libraries, and compilesdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.littleb01s"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.littleb01s.ashasakhichat"
         minSdk = 28
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
